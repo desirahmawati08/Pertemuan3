33bcd0974b086f25c56d7dd662477b5931b6a842
FCM <rahmawatidesi730@gmail.com>
2022-03-25 15:25:45 +0700

    initorder

--- src/main/kotlin/id/ac/polbeng/depandi/tes_kelas/InitOrder.kt
+++ src/main/kotlin/id/ac/polbeng/depandi/tes_kelas/InitOrder.kt
@@ -2,6 +2,9 @@ package id.ac.polbeng.depandi.tes_kelas
 
 class InitOrder(name: String) {
     val firstProperty = "First property: $name".also(::println)
+
+    //blok inisialisasi diawalidengan key init. selama instansi sebuah objek,
+    //urutan eksekusi blok inisialisasi sama dengan urutan yang ada pada tubuh kelas, berselangkan inisialisasi propertynya.
     init {
         println("First initializer block that prints ${name}")
     }

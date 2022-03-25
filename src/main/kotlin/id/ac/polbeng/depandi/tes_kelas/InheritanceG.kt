08c9f496c18293cb9dfe7b56ee202875bb6061e2
FCM <rahmawatidesi730@gmail.com>
2022-03-25 16:25:20 +0700

    inheritanceG

---
+++ src/main/kotlin/id/ac/polbeng/depandi/tes_kelas/InheritanceG.kt
@@ -0,0 +1,28 @@
+package id.ac.polbeng.depandi.tes_kelas
+
+open class Base(val name: String) {
+    init {
+        println("Initializing Base")
+    }
+    open val size: Int = name.length.also {
+        println("Initializing size in Base: $it")
+    }
+}
+
+class Derived(name: String, val lastName: String) :
+    Base(name.capitalize().also { println("Argument for Base: $it") }) {
+    init {
+        println("Initializing Derived")
+    }
+    override val size: Int = (super.size + lastName.length).also {
+        println("Initializing size in Derived: $it")
+    }
+}
+
+fun main(){
+    val turunan = Derived("Desi", "Rahmawati")
+    turunan.size
+}
+
+//Menurunkan urutan inisialisasi kelas turunan Selama pembuatan sebuah instance baru dari
+// kelas turunan, inisialisasi kelas induk diselesaikan terlebih dahulu pada langkah pertama (hanya mendahulukan evaluasi dari argument untuk konstruktor kelas induk) dan demikian terjadi sebelum inisialisasi logika dari kelas turunan yang berjalan.
